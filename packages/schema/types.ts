478fc6c206704c11422ddf8837badb1c52daea5d
Vagarth <vagarth419@gmail.com>
2025-02-27 12:04:11 +0530

    feat: create a zod schema to train model

---
+++ packages/schema/types.ts
@@ -0,0 +1,11 @@
+import { z } from 'zod'
+
+export const TrainModel = z.object({
+    name: z.string(),
+    type: z.enum(["Man", "Woman", "Other"]),
+    age: z.number(),
+    ethinicity: z.enum(["White", "Black", "American", "Indian", "East Asian"]),
+    eyeColor: z.enum(["Brown", "Black", "Hazel", "Blue", "Green", "Gray"]),
+    bald: z.boolean(),
+    images: z.array(z.string())
+})
