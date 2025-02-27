26fe02872236f46c96e9ac15865a92699930b38e
Vagarth <vagarth419@gmail.com>
2025-02-27 11:51:29 +0530

    feat: create a skeleton express server

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,1 +1,29 @@
 import express from 'express';
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.post('/ai/training', (req, res) => {
+
+})
+
+app.post('/ai/generate', (req, res) => {
+
+})
+
+app.post('/pack/generate', (req, res) => {
+
+})
+
+app.get('/pack/bulk', (req, res) => {
+
+})
+
+app.get('/image', (req, res) => {
+
+})
+
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`)
+})
