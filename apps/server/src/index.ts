b2f3bae3ef2f6f2ca53863c44a604b43b7d4ec1d
Vagarth <vagarth419@gmail.com>
2025-03-03 21:20:37 +0530

    add userId to model schema and create new db migration

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,14 +1,38 @@
+import { prismaClient } from './../../../packages/db/index';
+import { TrainModel } from './../../../packages/schema/types';
 import express from 'express';
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
+app.use(express.json())
 
-app.post('/ai/training', (req, res) => {
+app.post('/train', async(req, res) => {
+    const parsedBody = TrainModel.safeParse(req.body)
 
+    if (!parsedBody.success) {
+        res.status(411).json({
+            message: "Incorrect Inputs!"
+        })
+        return
+    }
+    const data = await prismaClient.model.create({
+        data: {
+            name: parsedBody.data.name,
+            type: parsedBody.data.type,
+            age: parsedBody.data.age,
+            ethinicity: parsedBody.data.ethinicity,
+            eyeColor: parsedBody.data.eyeColor,
+            bald: parsedBody.data.bald
+        }
+    })
+
+    res.json({
+        modelId: data.id
+    })
 })
 
-app.post('/ai/generate', (req, res) => {
+app.post('/generate', (req, res) => {
 
 })
 
--- packages/schema/types.ts
+++ packages/schema/types.ts
@@ -4,7 +4,7 @@ export const TrainModel = z.object({
     name: z.string(),
     type: z.enum(["Man", "Woman", "Other"]),
     age: z.number(),
-    ethinicity: z.enum(["White", "Black", "American", "Indian", "East Asian"]),
+    ethinicity: z.enum(["White", "Black", "American", "Indian", "EastAsian"]),
     eyeColor: z.enum(["Brown", "Black", "Hazel", "Blue", "Green", "Gray"]),
     bald: z.boolean(),
     images: z.array(z.string())
