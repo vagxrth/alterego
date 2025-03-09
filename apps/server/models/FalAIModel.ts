cead5836666441c30332fae830e7da83acb2f7ac
Vagarth <vagarth419@gmail.com>
2025-03-09 22:47:01 +0530

    feat: storing request id and response url in db

--- apps/server/models/FalAIModel.ts
+++ apps/server/models/FalAIModel.ts
@@ -7,21 +7,24 @@ export class FalAIModel extends BaseModel {
     }
 
     private async generateImage(prompt: string, tensorPath: string) {
-        const result = await fal.subscribe("fal-ai/flux-lora", {
+        const { request_id, response_url } = await fal.queue.submit("fal-ai/flux-lora", {
             input: {
                 prompt: prompt,
                 loras: [{ path: tensorPath, scale: 1 }]
             }
         });
-        return result;
+        return { request_id, response_url };
     }
 
     private async trainModel(zipURL: string, triggerWord: string) {
-        const { request_id } = await fal.queue.submit("fal-ai/flux-lora-fast-training", {
+        const { request_id, response_url } = await fal.queue.submit("fal-ai/flux-lora-fast-training", {
             input: {
-                images_data_url: zipURL
+                images_data_url: zipURL,
+                trigger_word: triggerWord
             },
             webhookUrl: `${process.env.WEBHOOK_BASE_URL}/fal-ai/webhook`,
         });
+
+        return { request_id, response_url };
     }
 }
