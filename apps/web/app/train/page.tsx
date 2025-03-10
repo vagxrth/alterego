ce4d78b1d6c8b33a9ff64b47ca9925484d485804
Vagarth <vagarth419@gmail.com>
2025-03-10 23:52:53 +0530

    feat: create a card component with all the fields necessary for model training

--- apps/web/app/train/page.tsx
+++ apps/web/app/train/page.tsx
@@ -1,22 +1,23 @@
 import React from 'react'
 import { Button } from "../../components/ui/button"
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
 } from "../../components/ui/card"
 import { Input } from "../../components/ui/input"
 import { Label } from "../../components/ui/label"
 import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
 } from "../../components/ui/select"
+import { Switch } from '../../components/ui/switch'
 
 const page = () => {
     return (
@@ -31,28 +32,66 @@ const page = () => {
                         <div className="grid w-full items-center gap-4">
                             <div className="flex flex-col space-y-1.5">
                                 <Label htmlFor="name">Name</Label>
-                                <Input id="name" placeholder="Name of your project" />
+                                <Input id="name" placeholder="Name of the model" />
                             </div>
                             <div className="flex flex-col space-y-1.5">
-                                <Label htmlFor="framework">Framework</Label>
+                                <Label htmlFor="type">Type</Label>
                                 <Select>
-                                    <SelectTrigger id="framework">
+                                    <SelectTrigger id='type'>
                                         <SelectValue placeholder="Select" />
                                     </SelectTrigger>
                                     <SelectContent position="popper">
-                                        <SelectItem value="next">Next.js</SelectItem>
-                                        <SelectItem value="sveltekit">SvelteKit</SelectItem>
-                                        <SelectItem value="astro">Astro</SelectItem>
-                                        <SelectItem value="nuxt">Nuxt.js</SelectItem>
+                                        <SelectItem value="man">Man</SelectItem>
+                                        <SelectItem value="woman">Woman</SelectItem>
+                                        <SelectItem value="others">Others</SelectItem>
                                     </SelectContent>
                                 </Select>
+                            </div>
+                            <div className="flex flex-col space-y-1.5">
+                                <Label htmlFor="age">Age</Label>
+                                <Input id="age" placeholder="Age of the model" />
+                            </div>
+                            <div className='flex flex-col space-y-1.5'>
+                                <Label htmlFor="ethinicity">Ethinicity</Label>
+                                <Select>
+                                    <SelectTrigger id='ethinicity'>
+                                        <SelectValue placeholder="Select" />
+                                    </SelectTrigger>
+                                    <SelectContent position="popper">
+                                        <SelectItem value="white">White</SelectItem>
+                                        <SelectItem value="black">Black</SelectItem>
+                                        <SelectItem value="american">American</SelectItem>
+                                        <SelectItem value="indian">Indian</SelectItem>
+                                        <SelectItem value="eastasian">East Asian</SelectItem>
+                                    </SelectContent>
+                                </Select>
+                            </div>
+                            <div className='flex flex-col space-y-1.5'>
+                                <Label htmlFor="eyecolor">Eye Color</Label>
+                                <Select>
+                                    <SelectTrigger id='eyecolor'>
+                                        <SelectValue placeholder="Select" />
+                                    </SelectTrigger>
+                                    <SelectContent position="popper">
+                                        <SelectItem value="brown">Brown</SelectItem>
+                                        <SelectItem value="black">Black</SelectItem>
+                                        <SelectItem value="hazel">Hazel</SelectItem>
+                                        <SelectItem value="blue">Blue</SelectItem>
+                                        <SelectItem value="green">Green</SelectItem>
+                                        <SelectItem value="gray">Gray</SelectItem>
+                                    </SelectContent>
+                                </Select>
+                            </div>
+                            <div className='flex flex-col space-y-1.5'>
+                                <Label htmlFor="bald">Bald</Label>
+                                <Switch />
                             </div>
                         </div>
                     </form>
                 </CardContent>
                 <CardFooter className="flex justify-between">
                     <Button variant="outline">Cancel</Button>
-                    <Button>Deploy</Button>
+                    <Button>Create Model</Button>
                 </CardFooter>
             </Card>
         </div>
