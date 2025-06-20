5793d8955d3108f4cec27066ccd644748091a50d
Vagarth <vagarth419@gmail.com>
2025-06-20 14:51:31 +0530

    feat(server): implement model training endpoint with request validation using TrainModelSchema

--- apps/server/index.ts
+++ apps/server/index.ts
@@ -1,11 +1,34 @@
 import express from "express";
+import type { Request, Response } from "express";
+import { TrainModelSchema, GenerateImageSchema, GenerateImagesFromPackSchema } from "@repo/types";
+import { prisma } from "@repo/db";
 
 const app = express();
 
+app.use(express.json());
+
+
 const PORT = process.env.PORT || 8080;
 
-app.post("/model/train", (req, res) => {
-  
+app.post("/model/train", async(req: Request, res: Response) => {
+  const parsedBody = TrainModelSchema.safeParse(req.body);
+  if (!parsedBody.success) {
+    res.status(411).json({ error: parsedBody.error.message });
+    return;
+  }
+
+  const { name, type, age, ethnicity, eyeColor, bald, images } = parsedBody.data;
+
+  await prisma.model.create({
+    data: {
+      name,
+      type,
+      age,
+      ethnicity,
+      eyeColor,
+      bald,
+    },
+  });
 });
 
 app.post("/model/generate", (req, res) => {
