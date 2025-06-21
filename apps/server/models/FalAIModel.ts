d00cffc1586f64756d340ef51cc892c57c83bbeb
Vagarth <vagarth419@gmail.com>
2025-06-21 07:52:13 +0530

    feat(server): update FalAIModel to include model training functionality and improve code formatting

--- apps/server/models/FalAIModel.ts
+++ apps/server/models/FalAIModel.ts
@@ -2,22 +2,27 @@ import { fal } from "@fal-ai/client";
 import { BaseModel } from "./BaseModel";
 
 export class FalAIModel extends BaseModel {
-  constructor() {
-    super();
-  }
+    constructor() {
+        super();
+    }
 
-  private async generateImage(prompt: string, tensorPath: string) {
-    const result = await fal.subscribe("fal-ai/flux-lora", {
-        input: {
-          prompt: prompt,
-          loras: [{ path: tensorPath, scale: 1 }],
-        }
-      });
+    private async generateImage(prompt: string, tensorPath: string) {
+        const result = await fal.subscribe("fal-ai/flux-lora", {
+            input: {
+                prompt: prompt,
+                loras: [{ path: tensorPath, scale: 1 }],
+            }
+        });
 
-      return result;
-  }
+        return result;
+    }
 
-  private async trainModel(inputImages: string[], triggerWord: string) {
-
-  }
+    private async trainModel(zipUrl: string, triggerWord: string) {
+        const { request_id } = await fal.queue.submit("fal-ai/flux-lora-fast-training", {
+            input: {
+                images_data_url: zipUrl,
+            },
+            webhookUrl: `${process.env.WEBHOOK_URL}/fal-ai/webhook`,
+        });
+    }
 }
