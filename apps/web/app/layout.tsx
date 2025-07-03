0ec305d037e209880d9a8b39b8e5bd9f081e6b4c
Vagarth <vagarth419@gmail.com>
2025-07-03 18:03:59 +0530

    feat(web): integrate Clerk authentication provider and add Appbar component to layout

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,7 +1,8 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Geist } from "next/font/google";
-
+import { ClerkProvider } from "@clerk/nextjs";
+import Appbar from "./components/Appbar";
 const geist = Geist({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -15,8 +16,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={geist.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en"> 
+        <body className={geist.className}>
+          <Appbar />
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
